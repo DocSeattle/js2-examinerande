126d177a753ab4b77e9e82cbe6067f489ef8fa40
DocSeattle <seattledraws@gmail.com>
2024-10-30 10:19:35 +0100

    rearranged element placement, added fields to form, further styling.

--- src/FormButton.jsx
+++ src/FormButton.jsx
@@ -6,6 +6,25 @@ export default function FormButton({ stateValue }) {
 
   return (
     <>
+      <input
+        id="toggleForm"
+        aria-label="Confirm Seats"
+        type="button"
+        onClick={() => {
+      const target = (document.getElementById("form-container"));
+      switch (hideForm && stateValue >= 0) {
+        case true:
+          target.removeAttribute("hidden");
+          hideForm = !hideForm;
+          return;
+        case false:
+          target.setAttribute("hidden", true);
+          hideForm = !hideForm;
+          return;
+      }
+    }
+        }
+      value="Book Seats" />
       <div hidden={hideForm} id="form-container">
         <form id="seatsForm">
           <input
@@ -15,6 +34,11 @@ export default function FormButton({ stateValue }) {
             aria-label="email input"
             placeholder="Your E-Mail:"
           ></input>
+          <div id="payment-info">
+          <input type="number" id="paymentinfo" aria-label="payment info" placeholder="card number"></input>
+          <input type="month" id="date" aria-label="expiration date" placeholder="MM/YY"/>
+          <input type="number" id="cvc" aria-label="cvc" placeholder="CVC"></input>
+          </div>
           <SelectionContext.Provider value={stateValue}>
             <div>
               <input
@@ -47,25 +71,6 @@ export default function FormButton({ stateValue }) {
           ></input>
         </form>
       </div>
-      <input
-        id="toggleForm"
-        aria-label="Confirm Seats"
-        type="button"
-        onClick={() => {
-      const target = (document.getElementById("form-container"));
-      switch (hideForm && stateValue >= 0) {
-        case true:
-          target.removeAttribute("hidden");
-          hideForm = !hideForm;
-          return;
-        case false:
-          target.setAttribute("hidden", true);
-          hideForm = !hideForm;
-          return;
-      }
-    }
-        }
-      value="Book Seats"></input>
     </>
   );
 }
